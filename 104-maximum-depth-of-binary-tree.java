03f8612cfa69cf8e5f6e05a212da10b258e716e0
riju568 <nix93826@gmail.com>
2025-10-12 18:04:45 +0530

    Create 104-maximum-depth-of-binary-tree.java

---
+++ 104-maximum-depth-of-binary-tree.java
@@ -0,0 +1,21 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return (root == null) ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(new Solution().maxDepth(root)); // Output: 3
+    }
+}
