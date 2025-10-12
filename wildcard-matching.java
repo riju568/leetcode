af72284e8ed6ff8d083cf92e0a5d225d8ec97896
riju568 <nix93826@gmail.com>
2025-10-12 17:37:19 +0530

    Create wildcard-matching.java

---
+++ wildcard-matching.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int i = 0, j = 0, star = -1, match = 0;
+        while (i < s.length()) {
+            if (j < p.length() && (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j))) {
+                i++; j++;
+            } else if (j < p.length() && p.charAt(j) == '*') {
+                star = j++;
+                match = i;
+            } else if (star != -1) {
+                j = star + 1;
+                i = ++match;
+            } else return false;
+        }
+        while (j < p.length() && p.charAt(j) == '*') j++;
+        return j == p.length();
+    }
+}
