bda409c5c3b3017e488e62618bad07b43495ab58
riju568 <nix93826@gmail.com>
2025-10-13 18:16:05 +0530

    Create concatenation-of-array.java

---
+++ concatenation-of-array.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length, ans[] = new int[2 * n];
+        for (int i = 0; i < 2 * n; i++) ans[i] = nums[i % n];
+        return ans;
+    }
+}
